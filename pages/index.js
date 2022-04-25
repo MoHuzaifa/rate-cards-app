5415f60a212b9465295e11acbc31466edbca6ec9
MoHuzaifa <muhammad.huzaifa@tintash.com>
2022-04-25 18:17:04 +0500

    Create menu & table and filter & display data as per user's choice

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,55 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
+import { Stack, Button } from '@mui/material';
+import SelectMenu from '../components/SelectMenu';
+import CustomTable from '../components/CustomTable';
+import loadCardsData from './api/data';
+import { FDDHeaderRow, LumpSumHeaderRow, menuItems } from '../helper';
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>RateCards</title>
-      </Head>
-      <h1>Let&apos;s get started</h1>
-    </div>
-  )
+	const [cardType, setCardType] = useState('');
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		loadCardsData(cardType).then(setData);
+	}, [cardType]);
+
+	const handleChange = (event) => {
+		setCardType(event.target.value);
+	};
+
+	const handleClick = () => {
+		alert('Begin Download');
+	};
+
+	return (
+		<Stack alignItems='center' gap={5}>
+			{/* Head Tag */}
+			<Head>
+				<title>RateCards</title>
+			</Head>
+
+			{/* Main Title */}
+			<h1>Rate Cards</h1>
+
+			{/* Drop Down Menu */}
+			<SelectMenu
+				menuItems={menuItems}
+				cardType={cardType}
+				handleChange={handleChange}
+			/>
+
+			{/* Custom Table */}
+			{cardType !== '' && (
+				<CustomTable
+					headerRow={cardType === 'FDD' ? FDDHeaderRow : LumpSumHeaderRow}
+					data={data}
+				/>
+			)}
+
+			<Button variant='contained' onClick={handleClick}>
+				Download
+			</Button>
+		</Stack>
+	);
 }
